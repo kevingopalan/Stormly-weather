3434315fe4eb5ded9ccef7922d1a60b49b14dd62
Kevin G <kevin.gopalan@vestal.k12.ny.us>
2024-03-06 17:41:12 -0500

    Updated UI

--- app/src/main/java/com/nano/snowcast/ui/home/HomeViewModel.java
+++ app/src/main/java/com/nano/snowcast/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("XXº");
+        mText.setValue("xxº");
     }
 
     public LiveData<String> getText() {
